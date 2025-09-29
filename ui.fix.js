2bd6d5447ab8c6438de20493c28959631e9af120
grassboy wu <grassboy@github.com>
2025-09-29 23:19:01 +0800

    再次挑戰三日前預約直接送出機制

--- ui.fix.js
+++ ui.fix.js
@@ -498,6 +498,8 @@ location.href = 'https://ticket.expo2025.or.jp/zh-tw/event_search/?id=MREBB6J6X2
                     'slot_id': json.slot_id
                 });
                 console.log('開始預約', json.event_code, json.slot_id);
+                fetch('https://ticket.expo2025.or.jp/event_search/?id='+localStorage.ticket_id+'&keyword=&screen_id=108&lottery='+(isToday(localStorage.date)? '5' : '4')+'&entrance_date='+localStorage.date);
+                await new Promise((r)=>setTimeout(r, 500));
                 var referrer = "https://ticket.expo2025.or.jp/zh-tw/event_time/?id="+localStorage.ticket_id+"&event_id="+json.event_code+"&screen_id=108&lottery="+(isToday(localStorage.date)? '5' : '4')+"&keyword=&event_type=0&reserve_id=&entrance_date="+localStorage.date;
                 try {
                     await fetch(referrer);
